3b9f2b057e1e32727fa8296b4dfff88c222ae36a
Jenniferyingni <jennyingni@gmail.com>
2023-05-01 00:35:15 -0400

    edit scatterplot tooltip

--- src/components/scatterplot/consumptionScatterplot.js
+++ src/components/scatterplot/consumptionScatterplot.js
@@ -257,9 +257,9 @@ const ConsumptionScatterplot = (props) => {
     
                     //tooltipBox.html("<span class='tooltipHeader'>" + d['Date'] + "</span></br>" + "<span class='tooltip-row-name'>Team: </span><span class='tooltip-opponent'>" + d['Team'] + "</span></br>" + "<span class='tooltip-row-name'>Win / Loss: </span><span class='tooltip-win'>Win" + "</span></br>" + "<span class='tooltip-row-name'>Opponent: </span><span class='tooltip-opponent'>" + d['Opponent'] + "</span>");
                     tooltipBox.html("<span class='tooltipHeader'>" + d['Name'] + "</span></br>" + 
-                        "<span class='tooltip-row-name'>Country: </span><span class='tooltip-win'>" + d['Country'] + 
-                        "</span></br>" + "<span class='tooltip-row-name'>Brand: </span><span class='tooltip-win'>" + d['Brand'] + 
-                        " </span></br>" + "<span class='tooltip-row-name'>ABV: </span><span class='tooltip-win'>" + d['ABV'] + 
+                        "<span class='tooltip-row-name'></span><span class='tooltip-win'>" + d['Total alcohol consumption per capita (liters of pure alcohol, projected estimates, 15+ years of age)'] + " liters" +
+                        "</span></br>" + "<span class='tooltip-row-name'>GDP: </span><span class='tooltip-win'>" + Number(d['GDP per capita, PPP (constant 2017 international $)']).toFixed(2) + 
+                        " </span></br>" + "<span class='tooltip-row-name'>Population: </span><span class='tooltip-win'>" + (Number(d['Population (historical estimates)'])/1000000).toFixed(2) + "M" +
                         "</span>");
                     // tooltipBox.show();
             
